f627bcdfe35ef54e20ccd0af58cf4086ad9edd52
Shehzad <shaxad.here@gmail.com>
2020-11-07 22:50:32 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "First Flutter App", home: SIForm()));
+  runApp(MaterialApp(title: "Work Finder", home: SIForm()));
 }
 
 class SIForm extends StatefulWidget {
@@ -12,13 +12,76 @@ class SIForm extends StatefulWidget {
 }
 
 class _SIFormState extends State<SIForm> {
-  // var _currency = ["Rupee", "Dollar", "Pound"];
+  final _minimumPadding = 5.0;
+  var _currency = ["Rupee", "Dollar", "Pound"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("First App")),
-      body: Container(child: Column(children: <Widget>[])),
+      appBar: AppBar(title: Text("Work Finder")),
+      body: Container(
+          margin: EdgeInsets.all(_minimumPadding * 2),
+          child: Column(children: <Widget>[
+            getImageAsset(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      hintText: "yourname@domain.com",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter your password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))))),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: "Name",
+                            hintText: "Your Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(0.5))))),
+                Expanded(
+                  child: DropdownButton<String>(
+                    items: _currency.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    value: "Rupees",
+                    onChanged: (String newValueSelected) {},
+                  ),
+                )
+              ],
+            )
+          ])),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/logo.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
     );
   }
 }
